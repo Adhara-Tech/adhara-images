fc8e9d05885e40a2f6c9b91c46076161c6ec5f45
Fernando Llaca <ferllarom@gmail.com>
2021-04-20 10:47:02 +0200

    Fix getModifiedFiles scripts to check all changes except deleted files

--- scripts/getModifiedFiles.sh
+++ scripts/getModifiedFiles.sh
@@ -6,4 +6,4 @@ if [[ "$BRANCH" == "master" ]]; then
   revision='~1'
 fi
 ignorechanges='scripts\|\.circleci\|Makefile\|\.gitignore\|README.md\|CODEOWNERS'
-git diff origin/master${revision}...HEAD --name-only --diff-filter=A | sort -u | grep -v -e ${ignorechanges} | uniq
+git diff origin/master${revision}...HEAD --name-only --diff-filter=d | sort -u | grep -v -e ${ignorechanges} | uniq
